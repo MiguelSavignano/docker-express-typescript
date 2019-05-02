82ab6efad8f34ed53cf22d09260c0afaee6db8ab
Miguel Savignano <miguel.savignano@the-cocktail.com>
2019-05-02 03:11:17 +0200

    .

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-docker build -t docker-express-typescript .
-docker run --rm -e PORT=3000 -p 3000:3000 docker-express-typescript
+docker build -t nodejs-app .
+docker run --rm -e PORT=3000 -p 3000:3000 nodejs-app
