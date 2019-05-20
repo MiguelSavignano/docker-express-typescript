731324c8e51b306182fa30d011c847ccdee0c69e
Miguel Savignano <miguel.savignano@the-cocktail.com>
2019-05-21 00:58:27 +0200

    azure error logs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,7 @@
+import * as appInsights from "applicationinsights";
+if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
+  appInsights.setup().start();
+}
 import app from "./app";
 
 const port = process.env.PORT || 3000;
